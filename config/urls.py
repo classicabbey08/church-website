bd79c66d7f5db27ecf3e88fc6e7d16caec171c4a
classicabbey08 <classicabbey08@gmail.com>
2026-07-24 19:22:35 +0100

    Added gallery page

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home, name="home"),
+
+    path(
+        "",
+        TemplateView.as_view(template_name="home.html"),
+        name="home"
+    ),
+
+    path(
+        "about/",
+        TemplateView.as_view(template_name="about.html"),
+        name="about"
+    ),
+    path("gallery/", TemplateView.as_view(template_name="gallery.html"), name="gallery"),
 ]
