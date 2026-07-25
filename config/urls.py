dfbddfda50aba7686997a8a493bc5f8e58b776ce
classicabbey08 <classicabbey08@gmail.com>
2026-07-25 16:30:48 +0100

    Complete church website version 1

--- config/urls.py
+++ config/urls.py
@@ -16,5 +16,24 @@ urlpatterns = [
         TemplateView.as_view(template_name="about.html"),
         name="about"
     ),
-    path("gallery/", TemplateView.as_view(template_name="gallery.html"), name="gallery"),
+
+    path("gallery/", 
+         TemplateView.as_view(template_name="gallery.html"), 
+         name="gallery"),
+
+    path("leadership/", 
+         TemplateView.as_view(template_name="leadership.html"), 
+         name="leadership"),
+
+    path("events/",
+          TemplateView.as_view(template_name="events.html"), 
+          name="events"),
+
+    path("donate/",
+          TemplateView.as_view(template_name="donate.html"),
+            name="donate"),
+
+    path("contact/", 
+         TemplateView.as_view(template_name="contact.html"),
+           name="contact"),
 ]
