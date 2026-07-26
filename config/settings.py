e55f1957af49b3c657e386fbbc9086cbee41a56e
classicabbey08 <classicabbey08@gmail.com>
2026-07-26 19:12:29 +0100

    Configure Django for Render

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-esacd3+p2rt3#wx81!ws&=hrif=uic*dwgnb#bo35ng$xzh@cv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
